0d357f3847b5552f5908c443b152096979ecda6a
Yaman-Katalan <23040456@student.ltuc.com>
2024-06-01 23:49:55 +0000

    abc

--- python/code_challenges/stack_queue/challenge01/challenge01.py
+++ python/code_challenges/stack_queue/challenge01/challenge01.py
@@ -5,21 +5,33 @@ class MyQueue:
         self.stack2 = []
 
     def push(self, x: int) -> None:
+        """
+        This method is used to add new value to the queue.
+        """
         self.stack1.append(x)
 
     def pop(self) -> int:
+        """
+        This method is used to remove a value from the queue from bottom.
+        """
         if not self.stack2:
             while self.stack1:
                 self.stack2.append(self.stack1.pop())
         return self.stack2.pop()
 
     def peek(self) -> int:
+        """
+        This method is used to return the first value added.
+        """
         if not self.stack2:
             while self.stack1:
                 self.stack2.append(self.stack1.pop())
         return self.stack2[-1]
 
     def empty(self) -> bool:
+        """
+        This method is used to check whther the queue is empty or not.
+        """
         return not self.stack1 and not self.stack2
 
     def __str__(self):
